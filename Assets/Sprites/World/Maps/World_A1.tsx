a2f1c71d9f49d59fb670609bdf52c548fbb30cd7
edwyn mckie <37442282+DarkShinobi100@users.noreply.github.com>
2019-02-27 20:28:50 +0000

    map for Level 2 now created and imported into unity

---
+++ Assets/Sprites/World/Maps/World_A1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="World_A1" tilewidth="48" tileheight="48" tilecount="192" columns="16">
+ <image source="../Tilesets/World_A1.png" width="768" height="576"/>
+</tileset>
---
+++ Assets/Sprites/World/Maps/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="48" tileheight="48" tilecount="416" columns="26">
+ <image source="../Tilesets/tileset.png" width="1280" height="768"/>
+</tileset>
