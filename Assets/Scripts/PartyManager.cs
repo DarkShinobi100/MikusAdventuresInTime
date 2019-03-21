99a135adbaf309a51922259589ed06ee66e1ea32
edwyn mckie <37442282+DarkShinobi100@users.noreply.github.com>
2019-03-21 00:08:47 +0000

    Revert "Revert "party manager added to player party""
    This reverts commit 47a427019553d96947f9331699153044e4b34001.

---
+++ Assets/Scripts/PartyManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PartyManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject player1, player2, player3, player4, player5;
+
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+    private void Update()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "Battle1")
+        {
+            player1.SetActive(true);
+            player2.SetActive(true);
+            player3.SetActive(false);
+            player4.SetActive(false);
+            player5.SetActive(false);
+        }
+        else if (scene.name == "Battle2")
+        {
+            player1.SetActive(true);
+            player2.SetActive(false);
+            player3.SetActive(true);
+            player4.SetActive(false);
+            player5.SetActive(false);
+        }
+        else if (scene.name == "Battle3")
+        {
+            player1.SetActive(true);
+            player2.SetActive(false);
+            player3.SetActive(false);
+            player4.SetActive(true);
+            player5.SetActive(false);
+        }
+        else if (scene.name == "Battle4")
+        {
+            player1.SetActive(true);
+            player2.SetActive(false);
+            player3.SetActive(false);
+            player4.SetActive(true);
+            player5.SetActive(false);
+        }
+        else if (scene.name == "Battle5")
+        {
+            player1.SetActive(true);
+            player2.SetActive(false);
+            player3.SetActive(false);
+            player4.SetActive(false);
+            player5.SetActive(false);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
