1524c574f9c739477d40d1f2c120142b2745bde8
edwyn mckie <37442282+DarkShinobi100@users.noreply.github.com>
2019-03-29 17:52:13 +0000

    Start screen updated

--- Assets/Scripts/RotateClock.cs
+++ Assets/Scripts/RotateClock.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class RotateClock : MonoBehaviour {
     [SerializeField]
     private float speed;
-
+    [SerializeField]
+    private GameObject target;
 
     // Update is called once per frame
     void Update () {
-        this.transform.Rotate( new Vector3(0, 0, speed * Time.deltaTime));
+        this.transform.RotateAround(target.transform.position , new Vector3(0, 0, speed * Time.deltaTime), speed);
     }
 }
